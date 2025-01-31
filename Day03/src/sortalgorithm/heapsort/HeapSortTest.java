26d89385269f03b3e7f947d7aa3f56d759857201
Ankit Patel <yugap1806@gmail.com>
2025-01-31 10:50:07 +0530

    Heap sort Done

---
+++ Day03/src/sortalgorithm/heapsort/HeapSortTest.java
@@ -0,0 +1,21 @@
+package sortalgorithm.heapsort;
+import java.util.Scanner;
+public class HeapSortTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        HeapSort hs = new HeapSort();
+        hs.heapSort(arr);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
